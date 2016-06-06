d998da317ba8710cb22e9728b0710aa3fb7803d4
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 14:13:16 -0400

    Update FolioInterfaces.java

--- app/src/main/java/com/creativtrendz/folio/ui/FolioInterfaces.java
+++ app/src/main/java/com/creativtrendz/folio/ui/FolioInterfaces.java
@@ -1,60 +1,34 @@
 package com.creativtrendz.folio.ui;
 
 import android.webkit.JavascriptInterface;
-import android.widget.TextView;
-
 import com.creativtrendz.folio.activities.MainActivity;
 
 public class FolioInterfaces {
     private final MainActivity mContext;
+
 
     public FolioInterfaces(MainActivity c) {
         mContext = c;
     }
 
 
-    @JavascriptInterface
-    public void setNotificationNum(final String number) {
-        try {
-            final int num = Integer.parseInt(number);
-            mContext.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mContext.setNotificationNum(num);
-                }
-            });
 
-        } catch (NumberFormatException e) {
-            mContext.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mContext.setNotificationNum(0);
-                }
-            });
 
-        }
-    }
 
     @JavascriptInterface
-    public void getMessages(final String number) {
-        try {
-            final int num = Integer.parseInt(number);
-            mContext.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mContext.setMessagesNum(num);
-                }
-            });
-
-        } catch (NumberFormatException e) {
-            mContext.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mContext.setMessagesNum(0);
-                }
-            });
-
-        }
+    public void getNums(final String notifications, final String messages, final String requests, final String feed) {
+        final int notifications_int = FolioHelpers.isInteger(notifications) ? Integer.parseInt(notifications) : 0;
+        final int messages_int = FolioHelpers.isInteger(messages) ? Integer.parseInt(messages) : 0;
+        final int requests_int = FolioHelpers.isInteger(requests) ? Integer.parseInt(requests): 0;
+        final int feed_int = FolioHelpers.isInteger(feed) ? Integer.parseInt(feed): 0;
+        mContext.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mContext.setNotificationNum(notifications_int);
+                mContext.setMessagesNum(messages_int);
+                mContext.setFriendsNum(requests_int);
+                mContext.setNewsNum(feed_int);
+            }
+        });
     }
-
 }
