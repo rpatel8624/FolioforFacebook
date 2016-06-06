eb0106f63dad97902d85251c6ef21616a3e05db1
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 13:40:11 -0400

    Update Credits.java

--- app/src/main/java/com/creativtrendz/folio/fragments/Credits.java
+++ app/src/main/java/com/creativtrendz/folio/fragments/Credits.java
@@ -9,7 +9,7 @@ package com.creativtrendz.folio.fragments;
 
 import com.creativetrends.folio.app.R;
 import com.creativtrendz.folio.activities.FolioApplication;
-
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -21,99 +21,103 @@ import android.preference.PreferenceManager;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.text.Html;
 import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Credits extends PreferenceFragment {
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        
-        addPreferencesFromResource(R.xml.about_preferences);
+
+		addPreferencesFromResource(R.xml.about_preferences);
 
 		Context context = FolioApplication.getContextOfApplication();
-        PreferenceManager.getDefaultSharedPreferences(context);
+		PreferenceManager.getDefaultSharedPreferences(context);
 
-        {
+		{
 
-}
-
+		}
 
 
 //listener for bejan preference
-findPreference("bejan").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        Log.v("CreditsFragment", "bejan clicked");
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_bejan_link))));
-        return true;
-    }
-});
+		findPreference("bejan").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Log.v("CreditsFragment", "bejan clicked");
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_bejan_link))));
+				return true;
+			}
+		});
 
 
 //listener for nam preference
-	findPreference("nam").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-		@Override
-		public boolean onPreferenceClick(Preference preference) {
-      Log.v("CreditsFragment", "nam clicked");
-      startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_nam_link))));
-      return true;
-  }
-});
-
-
-	findPreference("fadev").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-	  @Override
-	  public boolean onPreferenceClick(Preference preference) {	      
-	      startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_fadev_link))));
-	      return true;
-	  }
-	});
-
-
-
-
-	findPreference("beta").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-	@Override
-	public boolean onPreferenceClick(Preference preference) {
-   
-    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_beta_link))));
-    return true;
-	}
-	});
-	
-	findPreference("google_plus").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-		@Override
-		public boolean onPreferenceClick(Preference preference) {
-	   
-	    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_google_plus_link))));
-	    return true;
-		}
+		findPreference("nam").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Log.v("CreditsFragment", "nam clicked");
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_nam_link))));
+				return true;
+			}
 		});
 
-	
+
+		findPreference("fadev").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.credits_fadev_link))));
+				return true;
+			}
+		});
 
 
-	//open about dialog
-	Preference preferenceabout = findPreference("about");
-	preferenceabout.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-		@Override
-		public boolean onPreferenceClick(Preference preference) {
-	  
-		AlertDialog.Builder builder =
-		       new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle);
-		            builder.setTitle(getResources().getString(R.string.about_header));
-		            builder.setMessage( Html.fromHtml(getResources().getString(R.string.about_text)));
-		            builder.setPositiveButton(getResources().getString(R.string.ok), null);
-		            builder.setNegativeButton(null, null);
-		            builder.show();
-	  
-		return true;
-  }
-});
+		//open about dialog
+		Preference preferenceabout = findPreference("about");
+		preferenceabout.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				AlertDialog.Builder builder =
+						new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle);
+				builder.setTitle(getResources().getString(R.string.about_header));
+				builder.setMessage(Html.fromHtml(getResources().getString(R.string.about_text)));
+				builder.setPositiveButton(getResources().getString(R.string.ok), null);
+				builder.setNegativeButton(null, null);
+				builder.show();
 
+				return true;
+			}
+		});
 
+		Preference preferencetrnaslators = findPreference("translators");
+		preferencetrnaslators.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				AlertDialog.Builder builder =
+						new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle);
+				builder.setTitle(getResources().getString(R.string.preference_category12));
+				WebView wv = new WebView(getActivity());
+				wv.loadUrl("http:\\jnsdesigns.webs.com/trans.htm");
+				wv.setWebViewClient(new WebViewClient() {
+					@Override
+					public boolean shouldOverrideUrlLoading(WebView view, String url) {
+						view.loadUrl(url);
+						return true;
+					}
+				});
 
-}
+				builder.setView(wv);
+				builder.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.dismiss();
+					}
+				});
+				builder.show();
 
+				return true;
+			}
+		});
+
+	}
 }
