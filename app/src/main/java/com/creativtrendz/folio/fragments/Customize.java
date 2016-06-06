d4b54950a9aa975c6fbc42281f63118b7f49d3ac
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 13:40:35 -0400

    Update Customize.java

--- app/src/main/java/com/creativtrendz/folio/fragments/Customize.java
+++ app/src/main/java/com/creativtrendz/folio/fragments/Customize.java
@@ -1,92 +1,81 @@
-// Copyright (C) 2014-2015 Jorell Rutledge/Creative Trends.
-//This file is originally apart of Folio for Facebook.
-//Copyright notice must remain here if you're using any part of this code.
-//Some code taken from Tinfoil for Facebook
-//Some code taken from Facebook Lite
-
-
 package com.creativtrendz.folio.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.creativetrends.folio.app.R;
 import com.creativtrendz.folio.activities.MainActivity;
 
-public class Customize extends PreferenceFragment {
+public class Customize extends PreferenceFragment implements Preference.OnPreferenceClickListener {
 
-
-    public static final String KEY_THEME = "theme_preference";
-   
-    ListPreference themePreference;
-    
-
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	
         super.onCreate(savedInstanceState);
-
-       
-        
         addPreferencesFromResource(R.xml.customize_preferences);
-        
-            	
-        
-        themePreference = (ListPreference) findPreference(KEY_THEME);
-        
+        Preference images = findPreference("no_images");
+        Preference people = findPreference("hide_people");
+        Preference fab = findPreference("show_fab");
+        Preference tap = findPreference("tap");
+        images.setOnPreferenceClickListener(this);
+        people.setOnPreferenceClickListener(this);
+        fab.setOnPreferenceClickListener(this);
+        tap.setOnPreferenceClickListener(this);
 
-       
-      
 
-        
-    findPreference("tap").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-        @Override
-        public boolean onPreferenceClick(Preference preference) {
-        	relaunch();
-            return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        String key = preference.getKey();
+        switch (key) {
+
+
+            case "theme_preference_fb":
+
+
+            case "no_images":
+
+                break;
+
+            case "allow_inside":
+
+                break;
+
+            case "hide_people":
+
+                break;
+
+            case "theme_preference":
+
+                break;
+
+            case "show_fab":
+
+                break;
+
+            case "tap":
+                refresh();
+                break;
+
+
         }
-    });
-    
-  
 
-    }    
-
-		private void relaunch() { 
-            Intent intent = new Intent(getActivity(), MainActivity.class);
-            intent.putExtra("apply_changes_to_app", true);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-		}
-		
-		
-      
-
-	
+        return false;
 
 
-		@Override
-		public void onStart() {
-		super.onStart();
-
-		}
+    }
 
 
-		@Override
-		public void onResume() {
-		super.onResume();
-
-    
-		}
-
-		@Override
-		public void onPause() {
-		super.onPause();
+    private void refresh() {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.putExtra("apply_changes_to_app", true);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
 
-	}
+    }
+}
 
-	}
+
