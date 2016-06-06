096b6a2f75ace008c801be406b7590ba165de47a
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 13:46:28 -0400

    Update FolioApplication.java

--- app/src/main/java/com/creativtrendz/folio/activities/FolioApplication.java
+++ app/src/main/java/com/creativtrendz/folio/activities/FolioApplication.java
@@ -5,10 +5,23 @@
 //Some code taken from Facebook Lite
 
 package com.creativtrendz.folio.activities;
-
 import android.app.Application;
 import android.content.Context;
-import android.content.ContextWrapper;
+import com.creativetrends.folio.app.R;
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+
+@ReportsCrashes(formUri = "",  // will not be used
+        mailTo = "bugs@creativetrendsapps.com",
+        mode = ReportingInteractionMode.DIALOG,
+        resToastText = R.string.crash_toast_text,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = R.drawable.ic_launcher,
+        resDialogTitle = R.string.crash_dialog_title,
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt
+)
 
 public class FolioApplication extends Application {
 
@@ -18,16 +31,15 @@ public class FolioApplication extends Application {
     public void onCreate() {
         mContext = getApplicationContext();
         super.onCreate();
-        
-    }   
+
+        ACRA.init(this);
+
+    }
 
     public static Context getContextOfApplication() {
         return mContext;
     }
 
-	public static ContextWrapper getInstance() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 }
