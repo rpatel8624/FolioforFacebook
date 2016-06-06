c9dc54d841479c579d54a412e155bac6fcfe80a3
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 14:09:22 -0400

    Update FolioListener.java

--- app/src/main/java/com/creativtrendz/folio/utils/FolioListener.java
+++ app/src/main/java/com/creativtrendz/folio/utils/FolioListener.java
@@ -1,10 +1,12 @@
 package com.creativtrendz.folio.utils;
 
+import android.Manifest;
 import android.app.DownloadManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -15,18 +17,22 @@ import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 
 import com.creativetrends.folio.app.R;
+import com.creativtrendz.folio.activities.FolioApplication;
 import com.creativtrendz.folio.activities.MainActivity;
 import com.creativtrendz.folio.fragments.Notify;
 import com.creativtrendz.folio.ui.FolioHelpers;
 import com.creativtrendz.folio.ui.FolioWebViewScroll;
 import com.github.clans.fab.FloatingActionMenu;
+import com.greysonparrelli.permiso.Permiso;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -37,6 +43,7 @@ import java.util.List;
 public class FolioListener implements FolioWebViewScroll.Listener {
     private static final int ID_SAVE_IMAGE = 0;
     private static final int ID_SHARE_IMAGE = 1;
+    private static final int REQUEST_STORAGE = 3;
     private static SharedPreferences preferences;
     private final FolioWebViewScroll mWebView;
     private final FloatingActionMenu FAB;
@@ -44,6 +51,7 @@ public class FolioListener implements FolioWebViewScroll.Listener {
     private final int mScrollThreshold;
     private final DownloadManager mDownloadManager;
     private final View mCoordinatorLayoutView;
+    private static Context context;
     public FolioListener(MainActivity activity, WebView view) {
         fActivity = activity;
         mCoordinatorLayoutView = activity.mCoordinatorLayoutView;
@@ -52,36 +60,17 @@ public class FolioListener implements FolioWebViewScroll.Listener {
         preferences = PreferenceManager.getDefaultSharedPreferences(activity);
         mScrollThreshold = activity.getResources().getDimensionPixelOffset(R.dimen.fab_scroll_threshold);
         mDownloadManager = (DownloadManager) fActivity.getSystemService(Context.DOWNLOAD_SERVICE);
+        context = FolioApplication.getContextOfApplication();
     }
 
     @Override
     public void onPageStarted(String url, Bitmap favicon) {
+
     }
 
     @Override
     public void onPageFinished(String url) {
-        List<String> uri_segments = Uri.parse(url).getPathSegments();
-        if (!(uri_segments.size() > 0 && uri_segments.get(0).equals("composer"))) {
-
-        }
-
-
-        int update_interval = Integer.parseInt(preferences.getString(Notify.KEY_PREF_UPDATE_INTERVAL, "5000"));
-
-
-        FolioHelpers.updateNotificationsService(mWebView, update_interval);
-
-
-        FolioHelpers.updateMessagesService(mWebView, update_interval);
-
-
-        if (!url.contains("/composer/")) {
-            fActivity.swipeRefreshLayout.setEnabled(true);
-        } else {
-            fActivity.swipeRefreshLayout.setEnabled(false);
-        }
-
-
+        FolioHelpers.updateNumsService(mWebView);
     }
 
 
@@ -144,30 +133,39 @@ public class FolioListener implements FolioWebViewScroll.Listener {
             public boolean onMenuItemClick(MenuItem item) {
                 int i = item.getItemId();
                 if (i == ID_SAVE_IMAGE) {
-                    // Save the image
-                    Uri uri = Uri.parse(result.getExtra());
-                    DownloadManager.Request request = new DownloadManager.Request(uri);
-
-                    // Set the download directory
-                    File downloads_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-                    if (!downloads_dir.exists()) {
-                        if (!downloads_dir.mkdirs()) {
-                            return false;
+                    Permiso.getInstance().requestPermissions(new Permiso.IOnPermissionResult() {
+                        @Override
+                        public void onPermissionResult(Permiso.ResultSet resultSet) {
+                            if (resultSet.areAllPermissionsGranted()) {
+                                Uri uri = Uri.parse(result.getExtra());
+                                DownloadManager.Request request = new DownloadManager.Request(uri);
+                                File downloads_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                                if (!downloads_dir.exists()) {
+                                    if (!downloads_dir.mkdirs()) {
+                                        return;
+                                    }
+                                }
+                                File destinationFile = new File(downloads_dir, uri.getLastPathSegment());
+                                request.setDestinationUri(Uri.fromFile(destinationFile));
+                                request.setVisibleInDownloadsUi(true);
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                mDownloadManager.enqueue(request);
+                                Snackbar.make(mCoordinatorLayoutView, R.string.download_complete, Snackbar.LENGTH_SHORT).show();
+                            } else {
+                                Snackbar.make(mCoordinatorLayoutView, R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                    File destinationFile = new File(downloads_dir, uri.getLastPathSegment());
-                    request.setDestinationUri(Uri.fromFile(destinationFile));
 
-                    // Make notification stay after download
-                    request.setVisibleInDownloadsUi(true);
-                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-
-                    // Start the download
-                    mDownloadManager.enqueue(request);
+                        @Override
+                        public void onRationaleRequested(Permiso.IOnRationaleProvided callback, String... permissions) {
+                            // TODO Permiso.getInstance().showRationaleInDialog("Title", "Message", null, callback);
+                            callback.onRationaleProvided();
+                        }
+                    }, Manifest.permission.WRITE_EXTERNAL_STORAGE);
                     return true;
                 } else if (i == ID_SHARE_IMAGE) {
+                    requestStorage();
                     final Uri uri = Uri.parse(result.getExtra());
-                    // Share image
                     Target target = new Target() {
                         @Override
                         public void onPrepareLoad(Drawable placeHolderDrawable) {
@@ -175,8 +173,8 @@ public class FolioListener implements FolioWebViewScroll.Listener {
 
                         @Override
                         public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom arg1) {
+                            requestStorage();
                             String path = MediaStore.Images.Media.insertImage(fActivity.getContentResolver(), bitmap, uri.getLastPathSegment(), null);
-
                             Intent shareIntent = new Intent(Intent.ACTION_SEND);
                             shareIntent.setType("image/*");
                             shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path));
@@ -187,14 +185,14 @@ public class FolioListener implements FolioWebViewScroll.Listener {
                         public void onBitmapFailed(Drawable errorDrawable) {
                         }
                     };
-
                     Picasso.with(fActivity).load(uri).into(target);
                     Snackbar.make(mCoordinatorLayoutView, R.string.context_share_image_progress, Snackbar.LENGTH_SHORT).show();
-                    return true;
-                }
-                    return false;
-                }
+                } else {
+                    Snackbar.make(mCoordinatorLayoutView, R.string.permission_denied, Snackbar.LENGTH_SHORT).show();
 
+                }return true;
+
+            }
         };
 
 
@@ -203,6 +201,18 @@ public class FolioListener implements FolioWebViewScroll.Listener {
             contextMenu.add(0, ID_SHARE_IMAGE, 0, R.string.context_share_image).setOnMenuItemClickListener(handler);
         }
 
+
+    }
+
+    private void requestStorage() {
+        String locationPermission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        int hasPermission = ContextCompat.checkSelfPermission(context, locationPermission);
+        String[] permissions = new String[] { locationPermission };
+        if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(fActivity, permissions, REQUEST_STORAGE);
+        } else{
+
+        }
 
     }
 
