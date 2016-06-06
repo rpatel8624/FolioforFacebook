a80b70a0fa71796da41bd9fc5bdf381be3ce119c
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 14:10:27 -0400

    Update FileOperation.java

--- app/src/main/java/com/creativtrendz/folio/utils/FileOperation.java
+++ app/src/main/java/com/creativtrendz/folio/utils/FileOperation.java
@@ -9,21 +9,6 @@ import java.io.InputStreamReader;
 
 public class FileOperation {
 
-    // read raw files to string (for css files)
-    public static String readRawTextFile(Context ctx, int resId) {
-        InputStream inputStream = ctx.getResources().openRawResource(resId);
-        InputStreamReader inputReader = new InputStreamReader(inputStream);
-        BufferedReader buffReader = new BufferedReader(inputReader);
-        String line;
-        StringBuilder text = new StringBuilder();
-        try {
-            while ((line = buffReader.readLine()) != null)
-                text.append(line);
-        } catch (IOException e) {
-            return " ";
-        }
-        return text.toString();
-    }
 
     
     public static void deleteCache(Context context) {
