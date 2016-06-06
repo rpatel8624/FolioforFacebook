b36b5373c2362f39b2fc213b5184bfa51b711f3f
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2016-06-06 14:08:32 -0400

    Update FolioReceiver.java

--- app/src/main/java/com/creativtrendz/folio/notifications/FolioReceiver.java
+++ app/src/main/java/com/creativtrendz/folio/notifications/FolioReceiver.java
@@ -7,8 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-
-import com.creativtrendz.folio.fragments.Notify;
+import android.util.Log;
+import com.creativtrendz.folio.ui.FolioHelpers;
+import net.grandcentrix.tray.TrayAppPreferences;
 
 
 public class FolioReceiver extends BroadcastReceiver {
@@ -20,21 +21,30 @@ public class FolioReceiver extends BroadcastReceiver {
 
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ctxt);
-        if (preferences.getBoolean("notifications_activated", false)
-                || preferences.getBoolean("messages_activated", false)) {
-            int interval = Integer.parseInt(preferences.getString(Notify.KEY_PREF_UPDATE_INTERVAL, "600000"));
-            mgr.setRepeating(AlarmManager.ELAPSED_REALTIME, 5000, interval, pi);
+        TrayAppPreferences trayPreferences = new TrayAppPreferences(ctxt);
 
+
+        trayPreferences.put("interval_pref", Integer.parseInt(preferences.getString("interval_pref", "1800000")));
+        trayPreferences.put("ringtone", preferences.getString("ringtone", "content://settings/system/notification_sound"));
+        trayPreferences.put("vibrate", preferences.getBoolean("vibrate", false));
+        trayPreferences.put("led_light", preferences.getBoolean("led_light", false));
+        trayPreferences.put("notifications_everywhere", preferences.getBoolean("notifications_everywhere", true));
+        trayPreferences.put("notifications_activated", preferences.getBoolean("notifications_activated", false));
+
+
+
+        if (preferences.getBoolean("notifications_activated", false) || preferences.getBoolean("messages_activated", false)&& !cancel) {
+            int interval = Integer.parseInt(preferences.getString("interval_pref", "1800000"));
+            mgr.setRepeating(AlarmManager.ELAPSED_REALTIME, 1800000, interval, pi);
         } else {
-
             mgr.cancel(pi);
 
         }
     }
+
 
     @Override
     public void onReceive(Context ctxt, Intent i) {
         scheduleAlarms(ctxt, false);
     }
 }
-
